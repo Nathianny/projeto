d67b6cf7349e75a56609c3ffbde42e87f36c8900
Nathianny Pereira <nathianny_pereira26@hotmail.com>
2023-02-02 12:51:27 -0300

    cubos

---
+++ index.js
@@ -0,0 +1,17 @@
+function colorirAmarelo(){
+    var div = document.getElementById('caixa1');
+
+    div.style.backgroundColor = "yellow";
+}
+
+function colorirAzul(){
+    var div = document.getElementById('caixa2');
+
+    div.style.backgroundColor = "blue";
+}
+
+function colorirLaranja(){
+    var div = document.getElementById('caixa3');
+
+    div.style.backgroundColor = "orange";
+}
